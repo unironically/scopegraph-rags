609aade5a786e492d3a60d1b55f7c59875162e9d
Luke <bessa028@umn.edu>
2025-10-06 09:56:02 -0500

    LMR2 and LMR3. LMR2: forward referencing, sequential imports. LMR3: forward referencing of variables and self-influencing imports

---
+++ lmr-scopegraphs/lmr2/lmr/nameanalysis1/Scope.sv
@@ -0,0 +1,25 @@
+grammar lmr2:lmr:nameanalysis1;
+
+--------------------------------------------------
+
+abstract production scopeNoData
+top::Scope ::=
+{ forwards to scope(datumNone()); }
+
+abstract production scopeVar
+top::Scope ::= name::String ty::Type
+{ forwards to scope(datumVar(name, ^ty)); }
+
+abstract production scopeMod
+top::Scope ::= name::String
+{ forwards to scope(datumMod(name)); }
+
+--------------------------------------------------
+
+abstract production datumVar
+top::Datum ::= name::String ty::Type
+{ forwards to datum(name); }
+
+abstract production datumMod
+top::Datum ::= name::String
+{ forwards to datum(name); }
